f35ee3a048d07ff43ae6263502681cc9b46264ca
Bryce Schumacher <trebuchet90@gmail.com>
2015-04-18 22:41:20 -0500

    simplified sand spawn for debug

--- Pharaoh/AI.cs
+++ Pharaoh/AI.cs
@@ -314,7 +314,7 @@ class AI : BaseAI
                 {
                     if (tile.Type == 0)
                     {
-                        if ((Math.Abs(tile.X - spawn.X) == 1 && Math.Abs(tile.Y - spawn.Y) == 0) || (Math.Abs(tile.Y - spawn.Y)==1 && Math.Abs(tile.X - spawn.X) == 0))
+                        if ((Math.Abs(tile.Y - spawn.Y)==1 && Math.Abs(tile.X - spawn.X) == 0))
                         {
 
                             tryTrap(tile, 5, mySarcophagiTiles, myScarabs, trapCount);
