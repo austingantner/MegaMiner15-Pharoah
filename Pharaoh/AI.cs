7572277b050d4f77e062b40c1b3fc0ca131cb1e1
Bryce Schumacher <trebuchet90@gmail.com>
2015-04-19 05:26:42 -0500

    deadends debug

--- Pharaoh/AI.cs
+++ Pharaoh/AI.cs
@@ -265,9 +265,37 @@ class AI : BaseAI
             int myScarabs = me.Scarabs;
             int sarcophagusCount = mySarcophagi.Count;
             List<Tile> mySarcophagiTiles = new List<Tile>();
+
+            List<Tile> deadEnds = new List<Tile>();
+            for (int i = 26; i < tiles.Length - 27; i++)
+            {
+                int neighbors = 1;
+                if (getTile(tiles[i].X + 1, tiles[i].Y).Type == 0)
+                {
+                    neighbors++;
+                }
+                if (getTile(tiles[i].X - 1, tiles[i].Y).Type == 0)
+                {
+                    neighbors++;
+                }
+                if (getTile(tiles[i].X, tiles[i].Y + 1).Type == 0)
+                {
+                    neighbors++;
+                }
+                if (getTile(tiles[i].X, tiles[i].Y - 1).Type == 0)
+                {
+                    neighbors++;
+                }
+                if (neighbors == 1)
+                {
+                    deadEnds.Add(tiles[i]);
+                }
+            }
+
+            
             // Find the first open tiles and place the sarcophagi there
             int placed = 0;
-            for (int i = 0; i < tiles.Length; i++)
+            for (int i = 0; i < 3; i++)
             {
                 int tileBase = 0;
                     if (placed == 0)
@@ -285,7 +313,7 @@ class AI : BaseAI
                     Tile tile;
                     //if (placed < 2)
                     //{
-                    tile = tiles[tileBase + i];
+                    tile = deadEnds[i];
                 //Tile tile = tiles[i];
 
                 // If the tile is on my side and is empty
@@ -308,35 +336,7 @@ class AI : BaseAI
             // Continue spawning traps until there isn't enough money to spend
             List<Tile> spawnTiles = getMySpawns();
 
-            Queue<Point> path = new Queue<Point>();
-
-            Tile paTile = getTile(5,5);
-
-            int tileIt = 100;
-            while (!(tiles[tileIt].Type == 0))
-            {
-                tileIt++;
-            }
-            paTile = getTile(10, 10);
-            int endX = tiles[tileIt].X;
-            int endY = tiles[tileIt].Y;
-            tileIt = 200;
-            while(!(tiles[tileIt].Type == 0))
-            {
-                tileIt++;
-                
-            }
-            int startX = tiles[tileIt].X;
-            int startY = tiles[tileIt].Y;
-            path = findPath(new Point(startX, startY), new Point(endX, endY));
-            // If a path exists then move forward on the path
-            while (path.Count > 0)
-            {
-                
-                Point nextMove = path.Dequeue();
-                me.placeTrap(nextMove.x, nextMove.y, 1);
-            }
-
+            
             Tile mercTile = getTile(mySarcophagiTiles[0].X+1, mySarcophagiTiles[0].Y);
             if (mercTile.Type == 0)
             {
